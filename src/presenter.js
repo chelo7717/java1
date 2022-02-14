5532d46b565dba718dbe1f15a84aa97dd01f30c7
Marcelo Salinas <chelo336@gmail.com>
2022-02-14 01:04:10 -0400

    multiplicador

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,12 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const div2 = document.querySelector("#resultado-div2");
+const form2 = document.querySelector("#multiplicar-form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,4 +15,15 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
-});
+  
+})
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div2.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
+  
+}
+);
